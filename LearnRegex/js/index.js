8cb9dd75bff5c79b6b6ca138258ce7ca61133a5a
baristutakli <baristutakli@hotmail.com>
2022-01-04 20:42:19 +0300

    Finished Regex course

--- LearnRegex/js/index.js
+++ LearnRegex/js/index.js
@@ -112,7 +112,7 @@ let myPattern25 ='/...(?=")/g';
 
 // lookbehind
 // (?<="). 
-let str6 ='src="url"';
+let str66 ='src="url"';
 // iki tırnak arasındaki urlyi seçme
 
 let myPattern26 ='/(?<=").+(?=")/g';
@@ -131,7 +131,7 @@ let str9 ="123 456 7890";
 let myptr3 ="/(\d{3})[ -]?(\d{3})[ -]?(\d{4})/g";
 
 // +90 123 456 7890
-let str9 ="+90 123 456 7890";
+let str99 ="+90 123 456 7890";
 let myptr4 ="(\+\d{2})?[ ]?\(?(\d{3})\)?[ -]?(\d{3})[ -]?(\d{4})"
 
 
@@ -143,3 +143,64 @@ let myptr5 ="(?<areaCode>\+\d{2})?[ ]?\(?(?<operator>\d{3})\)?[ -]?(?<main>\d{3}
 // ilk grubun ismi: areacode
 // ikinci: initial
 // son kısım: deneme
+let str10 ="14/02/2018\n"+
+"14-02-2018\n"+
+"14.02.2018\n"+
+"14.02.18";
+// tarih doğrulama
+
+let myptr55="/(?<gun>\d{2})[\/\-\.](?<ay>\d{2})[\/\-\.](?<yıl>\d{2,4})/gm";
+
+// Url ve title ayrıştırma
+
+// [https://www.videosinif.com~videosinif]
+// [https://www.kablosuzkedi.com,kablosuzkedi]
+// [https://www.youtube.com/kablosuzkedi|kablosuzkedi youtube kanalı]
+
+// İsimlendirme için ?<isim> yapısını kullanıyoruz
+let myptr6="/(?<url>(?=h).+(?=[,|~]))[,|~](?<title>(?<=[,|~]).+(?=\]))/g";
+
+// ikinci yol
+
+let myptrn7="/(?<=\[)(?<url>.*)(?=[~,\|])[~,\|](?<=[~,\|])(?<title>.*)(?=\])/gm"
+
+
+// Key value ayrımı yapma
+
+// Name: Gokhan
+// LastName: Kandemir
+// Address: Adana
+// Age: 33
+// Married: Yes
+
+let myptrn11 ="/(?<key>(?=[a-zA-Z]).+(?=:))[:](?<value>(?<=:).+(?<=\w))/gm"
+
+
+// Email validasyonu
+// gokhan@gkandemir.com adresinden güzel bir email aldim. peki bu .com uzantılı email adreslerinden çektiğimiz nedir be kardeşim. onunla beraber delphixdfd@gmail.com diye ayri bir ergen zamanlarimda aldigim email adresi de mevcut :D
+
+let myptrn12="/(\w+@\w+\.[a-zA-Z]{2,})/gm";
+
+// URL valisadyonu
+
+// burada bir ton web sayfası var. https://www.google.com bunlardan bir tanesi. Neden olduğunu bilmiyorum ama http://www.test123.space de bunlardan bir tanesi. Oldukça güzel bir web sayfası daha var burada www.kablosuzkedi.com uzun zamandan beri güncellenmemiş fakat yine de bilgiler işe yarayabilir. Fakat video izlemek isterseniz youtube.com da buna uyan diğer bir güzel web sayfası
+
+let myptrn13 =/(https?:\/\/)?(www)?\.?([a-zA-Z0-9]+)(\.[a-zA-Z]{2,})/gm;
+
+
+// HTML Tag Ayıklama
+let myptrn14 =  /(?<hashtag>(?=<).+(?=>))/gm;
+
+// Hashtag Ayıklama
+
+// #[a-zA-Z0-9işüğçöı]+
+
+// Javascript Regex
+ const regex = /#[a-zA-Z0-9şığüçö]+/gm;
+// const regex = /(?<hashtag>(?=<).+(?=>))/gm;
+
+const newstr = `Regex için video hazırlıyorum. #Regex ile çözümlemek için Bana uğraştığınız merak ettiğiniz metinleri yazabilir misiniz? Mesela #Web sayfasındaki <body></body> #tag 'leri arasındaki bilgileri almak gibi. Bu #kolay tabi :) #Derdimianlatabilmişimdirumarım :) #360dayscleancode`;
+
+// console.log(str.match(regex));
+
+newstr.match(regex).forEach(h => console.log(h));
