4f91912b5ec62a6d64fdc5d508ad2068e7639f8a
baristutakli <baristutakli@hotmail.com>
2022-01-05 12:16:57 +0300

    MVC_Okul, add student,delete,show details,list

---
+++ MVC_Okul/Controllers/OgrenciController.cs
@@ -0,0 +1,108 @@
+﻿using MVC_Okul.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Okul.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        // Veri tabanı yerine kullandık
+        public static List<Ogrenci> Ogrenciler = new List<Ogrenci>() { new Ogrenci {Numara=1,FirstName="Barış",LastName="Tutakli" ,Sinif="NA203"} };
+        // GET: Ogrenci
+        // Öğrenci listesini yazdırsın. Yanlarında detay, Düzenleme, silme bağlantıalrı birlikte olsun
+
+        private Ogrenci _Ogr { get; set; }
+
+        public Ogrenci Ogr
+        {
+            get
+            {
+                if (this._Ogr==null)
+                {
+                    this._Ogr = new Ogrenci();
+                   
+                }
+                return this._Ogr;
+            }
+            set
+            {
+                this._Ogr = value;
+            }
+        }
+        public OgrenciController()
+        {
+            if (Ogrenciler.Count==0)
+            {
+                Ogrenciler.Add(Ogr);
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View(Ogrenciler);
+        }
+        public ActionResult Ekle()
+        {
+            return View(Ogr);
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(Ogrenci ogrenci)
+        {
+            Ogrenciler.Add(ogrenci);
+           return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil(int ogrNo)
+        {
+            
+
+            try
+            {
+                Ogrenci ogr = Ogrenciler.Find(Ogrenci => Ogrenci.Numara == ogrNo);
+                Ogrenciler.Remove(ogr);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detay(int ogrNo)
+        {
+
+            try
+            {
+                Ogrenci ogr = Ogrenciler.Find(Ogrenci => Ogrenci.Numara == ogrNo);
+                return View(ogr);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+        public ActionResult Duzenle(int ogrNo,Ogrenci ogrenci)
+        {
+            try
+            {
+                Ogrenci ogr = Ogrenciler.Find(Ogrenci => Ogrenci.Numara ==ogrNo);
+                ogr.FirstName = ogrenci.FirstName;
+                ogr.LastName = ogrenci.LastName;
+                ogr.Sinif = ogrenci.Sinif;
+                return View(ogr);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+      
+
+
+    }
+}
---
+++ MVC_Okul/Models/Ogrenci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Okul.Models
+{
+    public class Ogrenci
+    {
+        public  int Numara { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Sinif { get; set; }
+
+        private static int NSayac = 1;
+        public Ogrenci()
+        {
+            NSayac += 1;
+            this.Numara = NSayac;
+            
+        }
+    }
+}
